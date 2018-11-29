9676706bed4935e4e4e7f775c2d810b70141f6d0
Drew7smith <43586956+Drew7smith@users.noreply.github.com>
2018-11-28 22:30:58 -0500

    Update shooter.py

--- shooter.py
+++ shooter.py
@@ -4,8 +4,7 @@ from gpiozero import LED, Button
 from time import sleep
 from random import uniform
 import sys
-import RPi.GPIO as GPIO
-import time
+
 
 #assign pins to LED and buttons
 led = LED(4)
@@ -20,7 +19,7 @@ right_name = input('right player name is: ')
 led.on()
 
 #randomized wait time for LED to flash within range 3 - 8 seconds
-sleep(uniform(3,8))
+sleep(uniform(5,10))
 
 #turn off LED
 led.off()
@@ -52,25 +51,4 @@ def pressed(button):
 right_button.when_pressed=pressed
 left_button.when_pressed=pressed
 
-#add in a timer, to work out how long it took the players to press the button after the LED turned off
-GPIO.setmode(GPIO.BCM)
-#indicate which pins we will use in these variables
-right_button = Button(15)
-left_button = Button(14)
-led_pin = 4
-#setup the LED pin as an out pin
-GPIO.setup(led_pin,GPIO.OUT)
-#setup the switch pin as a switch that is always on by default (thus IN)
-GPIO.setup(switch_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-try:
-    while True:
-        #since switch is always on by default, pressing button causes False
-        if GPIO.input(switch_pin)== False:
-            #print a message with the formatted current time (string format time method)
-            print("Button Pressed at "+time.strftime("%Y/%m/%d - %H:%M:%S"))
-            GPIO.output(led_pin,True) #turn on LED
-            time.sleep(2) #wait 2 seconds
-            GPIO.output(led_pin,False) #turn off LED
-finally:
-    print("cleaning up")
-    GPIO.cleanup()
+
